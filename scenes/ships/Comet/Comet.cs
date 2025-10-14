c30183847440d5870890489e46ef478094171dc6
payton <pdp9729@gmail.com>
2025-10-14 17:47:26 -0600

    Some organizational changes

--- scenes/ships/Comet/Comet.cs
+++ scenes/ships/Comet/Comet.cs
@@ -1,48 +1,5 @@
-using Godot;
-using System;
-
-public partial class Comet : RigidBody3D
+public partial class Comet : Ship
 {
-	private bool manned = true;
-	private float thrust = 500;
-	private float turn = 500;
-
-	public override void _IntegrateForces(PhysicsDirectBodyState3D state)
-	{
-		var input = Input.Singleton;
-		if(manned)
-		{
-			var force = Vector3.Zero;
-			var torque = Vector3.Zero;
-
-			if(input.IsActionPressed("thrust_forward"))
-			{
-				force.Z += 1.0f;
-			}
-			// TODO: other thrust things?
-			if(input.IsActionPressed("pitch_up"))
-			{
-				torque.X -= 1.0f;
-			}
-			if(input.IsActionPressed("pitch_down"))
-			{
-				torque.X += 1.0f;
-			}
-			if(input.IsActionPressed("yaw_left"))
-			{
-				torque.Y += 1.0f;
-			}
-			if(input.IsActionPressed("yaw_right"))
-			{
-				torque.Y -= 1.0f;
-			}
-			// TODO: roll
-
-			var rotation = GlobalBasis;
-			var absoluteForce = rotation * force * thrust;
-			var absoluteTorque = rotation * torque * turn;
-			ApplyCentralForce(absoluteForce);
-			ApplyTorque(absoluteTorque);
-		}
-	}
+	protected override float Thrust => 5000;
+	protected override float Turn => 5000;
 }
---
+++ scripts/Ship.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+public abstract partial class Ship : RigidBody3D
+{
+	[Export] protected bool Manned = true;
+    protected abstract float Thrust { get; }
+	protected abstract float Turn { get; }
+
+	public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+	{
+		var input = Input.Singleton;
+		if(Manned)
+		{
+			var force = Vector3.Zero;
+			var torque = Vector3.Zero;
+
+			if(input.IsActionPressed("thrust_forward"))
+			{
+				force.Z += 1.0f;
+			}
+			// TODO: other thrust things?
+			if(input.IsActionPressed("pitch_up"))
+			{
+				torque.X -= 1.0f;
+			}
+			if(input.IsActionPressed("pitch_down"))
+			{
+				torque.X += 1.0f;
+			}
+			if(input.IsActionPressed("yaw_left"))
+			{
+				torque.Y += 1.0f;
+			}
+			if(input.IsActionPressed("yaw_right"))
+			{
+				torque.Y -= 1.0f;
+			}
+			// TODO: roll
+
+			var rotation = GlobalBasis;
+			var absoluteForce = rotation * force * Thrust;
+			var absoluteTorque = rotation * torque * Turn;
+			ApplyCentralForce(absoluteForce);
+			ApplyTorque(absoluteTorque);
+		}
+	}
+}
